6e007ad381605ac35088fc40492409b7b2111726
Flamesabber24 <flamesabber@protonmail.com>
2022-03-05 10:04:48 +0530

    json of songs

---
+++ src/stores.js
@@ -0,0 +1,10 @@
+import { writable } from "svelte/store";
+
+export const songs = writable(
+  {
+    "mudhal nee mudivum nee": "btWnZxF-Hck",
+    "arabic kuthu": "9nRvqemGydw",
+    "sirivennela": "-19EvIcr9ZA",
+    "uyire": "-hsdXiwA4c0"
+  }
+)
