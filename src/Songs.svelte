974beaeda5fe76679c2883f859410027a1cdfa02
Flamesabber24 <flamesabber@protonmail.com>
2022-03-15 15:30:56 +0530

    sorted songs list

--- src/Songs.svelte
+++ src/Songs.svelte
@@ -4,25 +4,26 @@
   import Song from "./Song.svelte";
   import Prompt from "./Prompt.svelte";
 
+  const comparator = (a, b) => a > b;
+
   let songsObj;
 
   const unsubscribe = songs.subscribe((songs) => {
     songsObj = JSON.parse(songs);
   });
 
-  let i = 0;
   let songsList = [];
   for (const song of Object.keys(songsObj)) {
-    songsList.push({ id: i, name: song });
-    i++;
+    songsList.push(song);
   }
 
-  const removeSong = (res, id) => {
-    if (res === true) {
-      delete songsObj[songsList[id].name];
+  songsList.sort(comparator);
 
-      songsList.splice(id, 1);
-      songsList = songsList;
+  const removeSong = (res, name) => {
+    if (res === true) {
+      delete songsObj[name];
+
+      songsList = songsList.filter((value, index, arr) => value != name);
 
       songs.subscribe((songs) => {
         localStorage.songs = JSON.stringify(songsObj);
@@ -36,8 +37,9 @@
     let newSong = Object.keys(songsObj);
     newSong = newSong[newSong.length - 1];
 
-    songsList = [...songsList, { id: i, name: newSong }];
-    i++;
+    songsList.push(newSong);
+    songsList.sort(comparator);
+    songsList = songsList;
   };
 
   onDestroy(unsubscribe);
@@ -46,11 +48,11 @@
 <div class="flex min-w-fit flex-col items-center content-around">
   <h3 class="text-2xl bg-zinc-900 text-white w-full z-10 text-center">Songs</h3>
   <div class="bg-gray-200 overflow-scroll songs">
-    {#each songsList as song (song.id)}
+    {#each songsList as song (song)}
       <div class="border-b-2 border-gray-300 flex flex-row pl-3">
         <Song
-          name={song.name}
-          on:remove={({ detail }) => removeSong(detail, song.id)}
+          name={song}
+          on:remove={({ detail }) => removeSong(detail, song)}
         />
       </div>
     {/each}
